05f6fe690b0fbb5019762a97da350a9a102bcea6
UChebNick <153133590+UChebNick@users.noreply.github.com>
2024-09-20 18:14:34 +0300

    Delete bot directory

--- bot/crypto_pay_lib/README.md
+++
@@ -1,5 +0,0 @@
-# crypto_pay_lib
-
-[crypto_pay_docs](https://help.crypt.bot/crypto-pay-api#createInvoice)
-
-[create_token](https://telegram.me/cryptobot)
--- bot/threading_start.py
+++
@@ -1,7 +0,0 @@
-import threading
-import bot, msg_utils
-import payment_utils
-
-threading.Thread(target=lambda: payment_utils.main()).start()
-threading.Thread(target=lambda: bot.polling(non_stop=True)).start()
-threading.Thread(target=lambda: msg_utils.main()).start()
